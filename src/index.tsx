3aa89f79d5b6512f5f4078d5e5a7e506444fdcc4
Хильченко Дмитрий Сергеевич <dkhilchenko@naumen.ru>
2022-06-14 00:43:49 +0500

    Диплом. React

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
+import {Provider} from "react-redux";
+import {setupStore} from "./redux/redux-store";
+import {BrowserRouter} from "react-router-dom";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+const store = setupStore()
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter basename={"/git-statistics/"}>
+                <App/>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
